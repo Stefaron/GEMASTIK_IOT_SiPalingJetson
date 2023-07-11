9de370262179ff2560201708aafc60350d7ca461
Stefaron Budhi Santosa <93313500+Stefaron@users.noreply.github.com>
2023-07-11 16:30:24 +0700

    membuat bounding box

--- app/src/main/java/com/example/fyp/Detector.kt
+++ app/src/main/java/com/example/fyp/Detector.kt
@@ -1,3 +1,62 @@
+//package com.example.fyp
+//
+//import android.graphics.Rect
+//import com.google.mlkit.common.model.LocalModel
+//import com.google.mlkit.vision.common.InputImage
+//import com.google.mlkit.vision.objects.ObjectDetection
+//import com.google.mlkit.vision.objects.ObjectDetector
+//import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
+//import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
+//import com.otaliastudios.cameraview.Frame
+//
+//class Detector {
+//
+//    lateinit var objectDetector: ObjectDetector
+//
+//    public fun load_model()
+//    {
+//        val options = ObjectDetectorOptions.Builder()
+//                .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
+//                .build()
+//        objectDetector = ObjectDetection.getClient(options)
+//    }
+//
+//    public fun load_custom_model()
+//    {
+//        val localModel = LocalModel.Builder()
+//            .setAssetFilePath("detect.tflite")
+//            .build()
+//
+//        val customObjectDetectorOptions =
+//            CustomObjectDetectorOptions.Builder(localModel)
+//                .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+//                .enableClassification()
+//                .setClassificationConfidenceThreshold(0.6f)
+//                .setMaxPerObjectLabelCount(5)
+//                .build()
+//
+//        val objectDetector =
+//            ObjectDetection.getClient(customObjectDetectorOptions)
+//    }
+//
+//    public fun detect(input: InputImage): Rect {
+//
+//        var result = Rect(0,0,0,0)
+//        objectDetector.process(input)
+//                .addOnSuccessListener { detectedObjects ->
+//                    for (detectedObject in detectedObjects) {
+//                        val boundingBox = detectedObject.boundingBox
+//                        val trackingId = detectedObject.trackingId
+//                        result = boundingBox
+//                    }
+//                }
+//                .addOnFailureListener { e ->
+//                }
+//
+//        return result
+//    }
+//}
+
 package com.example.fyp
 
 import android.graphics.Rect
@@ -5,24 +64,24 @@ import com.google.mlkit.common.model.LocalModel
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.objects.ObjectDetection
 import com.google.mlkit.vision.objects.ObjectDetector
+import com.google.mlkit.vision.objects.DetectedObject
+
 import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
 import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
 import com.otaliastudios.cameraview.Frame
 
 class Detector {
 
-    lateinit var objectDetector: ObjectDetector
+    private lateinit var objectDetector: ObjectDetector
 
-    public fun load_model()
-    {
+    fun load_model() {
         val options = ObjectDetectorOptions.Builder()
-                .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
-                .build()
+            .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
+            .build()
         objectDetector = ObjectDetection.getClient(options)
     }
 
-    public fun load_custom_model()
-    {
+    fun load_custom_model() {
         val localModel = LocalModel.Builder()
             .setAssetFilePath("detect.tflite")
             .build()
@@ -35,24 +94,16 @@ class Detector {
                 .setMaxPerObjectLabelCount(5)
                 .build()
 
-        val objectDetector =
-            ObjectDetection.getClient(customObjectDetectorOptions)
+        objectDetector = ObjectDetection.getClient(customObjectDetectorOptions)
     }
 
-    public fun detect(input: InputImage): Rect {
-
-        var result = Rect(0,0,0,0)
+    fun detect(input: InputImage, onDetectionResult: (List<DetectedObject>) -> Unit) {
         objectDetector.process(input)
-                .addOnSuccessListener { detectedObjects ->
-                    for (detectedObject in detectedObjects) {
-                        val boundingBox = detectedObject.boundingBox
-                        val trackingId = detectedObject.trackingId
-                        result = boundingBox
-                    }
-                }
-                .addOnFailureListener { e ->
-                }
-
-        return result
+            .addOnSuccessListener { detectedObjects ->
+                onDetectionResult(detectedObjects)
+            }
+            .addOnFailureListener { e ->
+                // Handle failure
+            }
     }
 }
